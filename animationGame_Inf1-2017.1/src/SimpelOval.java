f859d89bf2c8d2a2722db9c05fd2ba3cad44f512
Weber.Elias <Weber.Elias@SK-D43D7EB3B8E4>
2015-10-16 08:31:12 +0200

    commit

---
+++ animationGame_Inf1-2017.1/src/SimpelOval.java
@@ -0,0 +1,16 @@
+
+public class SimpelOval extends GeometricObject {
+
+	public SimpelOval (double w, double h, Vertex pos ){
+		super(w, h, pos);}
+	public SimpelOval (double w, double h, double x, double y){
+		super(w, h, new Vertex(x,y));}
+public double area (){return Math.PI*width*height/4;}
+	
+public String toString(){
+			return "SimpelOval("+super.toString()+")";}
+				
+}
+	
+	
+
