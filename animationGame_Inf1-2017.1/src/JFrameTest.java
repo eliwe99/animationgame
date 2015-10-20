57ed146b06b7e775230c8fda1c06e347f1293b11
Weber.Elias <Weber.Elias@SK-D43D7EB3B8E4>
2015-10-20 14:42:18 +0200

    commit

---
+++ animationGame_Inf1-2017.1/src/JFrameTest.java
@@ -0,0 +1,9 @@
+ import javax.swing.JFrame;
+ public class JFrameTest{
+ public static void main(String [] args) {
+ Frame f=new JFrame("hier könnte Ihre Werbung stehen");
+ f.setVisible(true);}}
+
+	
+	
+
